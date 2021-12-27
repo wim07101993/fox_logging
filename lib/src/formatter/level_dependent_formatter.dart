434371ebd64946c5dfc2e20d0f2b633d5de27479
Wim Van Laer <van.laer.wim@gmail.com>
2021-12-27 20:56:06 +0100

    added docs

--- lib/src/formatter/level_dependent_formatter.dart
+++ lib/src/formatter/level_dependent_formatter.dart
@@ -1,7 +1,12 @@
 import 'package:logging/logging.dart';
 import 'package:logging_extensions/src/formatter/log_record_formatter.dart';
 
+/// Selects a different formatter depending on the level of the [logRecord].
 class LevelDependentFormatter extends LogRecordFormatter {
+  /// Constructs a new  [LevelDependentFormatter] with a formatter for each
+  /// [Level].
+  ///
+  /// If no formatter is specified, the [defaultFormatter] is used.
   LevelDependentFormatter({
     required this.defaultFormatter,
     this.finest,
@@ -16,18 +21,41 @@ class LevelDependentFormatter extends LogRecordFormatter {
     this.all,
   });
 
+  /// Used when no formatter is specified for the level of the record to format.
   final LogRecordFormatter defaultFormatter;
+
+  /// Formatter used to format records with level [Level.FINEST].
   final LogRecordFormatter? finest;
+
+  /// Formatter used to format records with level [Level.FINER].
   final LogRecordFormatter? finer;
+
+  /// Formatter used to format records with level [Level.FINE].
   final LogRecordFormatter? fine;
+
+  /// Formatter used to format records with level [Level.CONFIG].
   final LogRecordFormatter? config;
+
+  /// Formatter used to format records with level [Level.INFO].
   final LogRecordFormatter? info;
+
+  /// Formatter used to format records with level [Level.WARNING].
   final LogRecordFormatter? warning;
+
+  /// Formatter used to format records with level [Level.SEVERE].
   final LogRecordFormatter? severe;
+
+  /// Formatter used to format records with level [Level.SHOUT].
   final LogRecordFormatter? shout;
+
+  /// Formatter used to format records with level [Level.OFF].
   final LogRecordFormatter? off;
+
+  /// Formatter used to format records with level [Level.ALL].
   final LogRecordFormatter? all;
 
+  /// Formats the [logRecord] using the [LogRecordFormatter] specified for
+  /// its [LogRecord.level].
   @override
   String format(LogRecord logRecord) {
     LogRecordFormatter? formatter;
