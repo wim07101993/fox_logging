8e2fb4cba2c096246444b460418d2e250b65ca1a
Wim Van Laer <wim07101993@users.noreply.github.com>
2023-02-11 18:46:44 +0100

    feat: Remove fluter fox logging to other repo
    * removed flutter fox logging
    
    * Delete .github/workflows/templates directory
    
    * Delete test_flutter_fox_logging.yml
    
    * Update and rename publish_fox_logging.yaml to publish.yaml
    
    * Update and rename test_fox_logging.yml to ensure_code_quality.yml

--- packages/flutter_fox_logging/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_fox_logging
-
-A widget library for the `fox_logging` package.
--- packages/flutter_fox_logging/test/mocks.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_fox_logging/flutter_fox_logging.dart';
-import 'package:mocktail/mocktail.dart';
-
-// ignore: avoid_implementing_value_types
-class MockIconData extends Mock implements IconData {}
-
-class MockLogsController extends Mock implements LogsController {}
