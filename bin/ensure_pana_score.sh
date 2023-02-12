dd3cc5a3d46a1439cb9ececefb0f4ea637c880da
Wim Van Laer <van.laer.wim@gmail.com>
2023-02-12 12:28:20 +0100

    fix: moved ensure pana

--- bin/ensure_pana_score.sh
+++ bin/ensure_pana_score.sh
@@ -10,6 +10,7 @@
 # `./ensure_pana_score.sh`
 
 PANA=$(pana . --no-warning)
+echo "PANA"
 PANA_SCORE=$(echo "$PANA" | sed -n "s/.*Points: \([0-9]*\)\/\([0-9]*\)./\1\/\2/p")
 echo "score: $PANA_SCORE"
 IFS='/'
